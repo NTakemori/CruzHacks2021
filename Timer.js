f8fb1becb124c4f26488e4e4a968328717af9cc9
Julian Shalaby <julianshalaby@gmail.com>
2021-01-16 18:24:05 -0800

    fixed small bug

--- Timer.js
+++ Timer.js
@@ -41,5 +41,5 @@ let resetTimer = (timerID = "timer", id) => {
     isOn[id]=false;
     minutes[id]=minsResetter[id];
     clearInterval(timer[id]);
-    document.getElementById(timerID).innerHTML = null;
+    document.getElementById(timerID).innerHTML = " 6";
 }
