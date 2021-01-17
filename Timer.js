edfa34fb1fb0efaa8ec4390f61e0e6453bea0dd9
Julian Shalaby <julianshalaby@gmail.com>
2021-01-16 18:08:14 -0800

    p

--- Timer.js
+++ Timer.js
@@ -1,7 +1,8 @@
-let minutes = [30*60, 60*60, 45*60];
-let isOn = [false, false, false];
-let firstClick = [true, true, true];
-let timer = [null, null, null];
+let minutes = [30*60, 60*60, 45*60, 20*60];
+let minsResetter = [30*60, 60*60, 45*60, 20*60];
+let isOn = [false, false, false, false];
+let firstClick = [true, true, true, true];
+let timer = [null, null, null, null];
 
 let useTimer = (timerID = "timer", id) => {
 
@@ -16,13 +17,12 @@ let useTimer = (timerID = "timer", id) => {
     else{
         isOn[id]=true;
         timer[id] = window.setInterval(function() {
-            myFunction(minutes[id], timerID, id);
+            myFunction(timerID, id);
         }, 1000); // every second;
     }
 }
 
-function myFunction(mins = 60, timerID = "timer", id) {
-    minutes[id] = mins;
+function myFunction(timerID = "timer", id) {
     let formattedMins = Math.floor(minutes[id]/60);
     let formattedSecs = Math.floor(minutes[id]%60);
     document.getElementById(timerID).innerHTML = formattedMins + " " + formattedSecs;
@@ -30,14 +30,14 @@ function myFunction(mins = 60, timerID = "timer", id) {
         minutes[id]--;
     }
     else if (minutes[id] === 0) {
-        minutes[id] = mins;
+        minutes[id] = minsResetter[id];
     }
 }
 
 let resetTimer = (timerID = "timer", id) => {
     isOn[id]=false;
     firstClick[id]=true;
-    minutes[id]=null;
+    minutes[id]=minsResetter[id];
     clearInterval(timer[id]);
     document.getElementById(timerID).innerHTML = null;
 }
