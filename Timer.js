d601181391bded55e686cb04ac16bb7fef7eb031
Julian Shalaby <julianshalaby@gmail.com>
2021-01-17 06:53:04 -0800

    Revert "Merge pull request #10 from NTakemori/SysTab"
    This reverts commit 915129e37086ef42dd9e4ca66a7adca527b8dfa0, reversing
    changes made to 5cee7df46661655ceaf4a5a77948d9ec2f2d40cc.

--- Timer.js
+++ Timer.js
@@ -1,10 +1,8 @@
 
 //meditation(every 2 hours for 5 min), stand up(every hour for 1 min),
 //look away(every 20 min for 20 sec), drink water(every 30 min for 30 sec)
-// let minutes = [120*60, 60*60, 20*60, 30*60];
-// let minsResetter = [120*60, 60*60, 20*60, 30*60];
-let minutes = [100, 100, 100, 100];
-let minsResetter = [100, 100, 100, 100];
+let minutes = [120*60, 60*60, 20*60, 30*60];
+let minsResetter = [120*60, 60*60, 20*60, 30*60];
 let intervalTime = [60*5, 60, 20, 30];
 let isOn = [false, false, false, false];
 let inInterval = [false, false, false, false];
@@ -26,9 +24,8 @@ let useTimer = (timerID = "timer", id) => {
         timer[id] = window.setInterval(function() {
             myFunction(timerID, id); 
             let css = ".circle_animation" + id.toString();
-            // console.log("hello");
-            // if( document.getElementsByClassName("circle_animation1")[0] )
-            //     document.getElementsByClassName("circle_animation1")[0].style.strokeDashoffset = 0; 
+            console.log(css);
+            document.getElementsByClassName(css).css('stroke-dashoffset', 0);
         }, 1000); // every second;
     }
 }
@@ -36,32 +33,19 @@ let useTimer = (timerID = "timer", id) => {
 function myFunction(timerID = "timer", id) {
     let formattedMins = Math.floor(minutes[id]/60);
     let formattedSecs = Math.floor(minutes[id]%60);
-    let h = "head" + id.toString();
-    let timePassed = minsResetter[id] - minutes[id];
-    let dist = ((timePassed+1)*(351/minsResetter[id]));
-    if (document.getElementsByClassName("yeet")[0]) {
-        document.getElementsByClassName("yeet")[0].innerHTML = formattedMins + ":" + formattedSecs;
-    }
-    if (document.getElementsByClassName("circle_animation1")) {
-        // console.log(timePassed);
-        document.getElementsByClassName("circle_animation1")[0].style.strokeDashoffset = dist;
-    }
-    // console.log("element found  ", elements[0]);
-    // elements[0].innerHTML 
+    let h = "H" + id.toString();
+    console.log("ID  ",  id);
+    let elements = document.getElementsByClassName("H0");
+    console.log("element found  ", elements[0]);
+    elements[0].innerHTML = formattedMins + " " + formattedSecs;
     // for( let i = 0; i < elements.length(); i++ ){
         
     // };
     if (minutes[id] > 0 ) { // so it doesn't go to -1
         minutes[id]--;
-        // document.getElementsByTa.text(formattedMins + ":" + formattedSecs);
+        document.getElementsByTa.text(formattedMins + ":" + formattedSecs);
         let css = ".circle_animation" + id.toString();
-        // if ( document.getElementsByClassName("yeet")[0] )
-        // let dist = 351-((minsResetter[id] - minutes[id]+1)*(351/minsResetter[id]));
-        // console.log(dist);
-        // if( document.getElementsByClassName("yeet")[0] ){
-        //     console.log("attempting to change")
-        //     document.getElementsByClassName("yeet")[0].style.strokeDashoffset = dist;     
-        // }          
+        document.getElementsByClassName(css).css('stroke-dashoffset', 351+((minsResetter[id] - minutes[id]+1)*(351/minsResetter[id])));               
 }
     else if (minutes[id] === 0) {
         if (inInterval[id] === false){
