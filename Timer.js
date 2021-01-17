42b1eabe38fbf4d34dcb5b109a6608393b0dec9f
Julian Shalaby <julianshalaby@gmail.com>
2021-01-16 17:57:11 -0800

    updated again

--- Timer.js
+++ Timer.js
@@ -1,86 +1,43 @@
-// let seconds;
-// let isOn = false;
-// let firstClick = true;
-// let timer;
-//
-// let useTimer = (secs = 60, timerID = "timer") => {
-//
-//     if (firstClick === true) {
-//         seconds = secs;
-//         firstClick=false;
-//     }
-//     if (isOn){
-//         isOn=false;
-//         clearInterval(timer);
-//     }
-//     else{
-//         isOn=true;
-//         timer = window.setInterval(function() {
-//             myFunction(seconds, timerID);
-//         }, 1000); // every second;
-//     }
-// }
-//
-// function myFunction(secs = 60, timerID = "timer") {
-//     seconds = secs;
-//     document.getElementById(timerID).innerHTML = seconds;
-//     if (seconds > 0 ) { // so it doesn't go to -1
-//         seconds--;
-//     }
-//     else if (seconds === 0) {
-//         seconds = secs;
-//     }
-// }
-//
-// let resetTimer = (timerID = "timer") => {
-//     isOn=false;
-//     firstClick=true;
-//     seconds=null;
-//     clearInterval(timer);
-//     document.getElementById(timerID).innerHTML = null;
-// }
+let minutes = [30*60, 60*60, 45*60];
+let isOn = [false, false, false];
+let firstClick = [true, true, true];
+let timer = [null, null, null];
 
-class timer {
-    constructor(seconds, TimerID) {
-        this.seconds = seconds;
-        this.isOn = false;
-        this.TimerID = TimerID;
-        this.timer
-        this.myFunction()
+let useTimer = (timerID = "timer", id) => {
+
+    if (firstClick[id] === true) {
+        minutes[id];
+        firstClick[id]=false;
     }
-
-    useTimer() {
-        console.log("Hertseers");
-        if (this.isOn){
-            this.isOn=false;
-            clearInterval(this.timer);
-        }
-        else{
-            this.isOn=true;
-            this.timer = window.setInterval(function() {
-                this.myFunction();
-            }, 1000); // every second;
-        }
+    if (isOn[id]){
+        isOn[id]=false;
+        clearInterval(timer[id]);
     }
-
-    myFunction() {
-        console.log("hi");
-        seconds = this.seconds;
-        document.getElementById(this.timerID).innerHTML = seconds;
-        if (this.seconds > 0 ) { // so it doesn't go to -1
-            this.seconds--;
-        }
-        else if (this.seconds === 0) {
-            this.seconds = secs;
-        }
-    }
-
-    resetTimer() {
-        this.isOn=false;
-        this.seconds=null;
-        this.clearInterval(timer);
-        document.getElementById(this.timerID).innerHTML = null;
+    else{
+        isOn[id]=true;
+        timer[id] = window.setInterval(function() {
+            myFunction(minutes[id], timerID, id);
+        }, 1000); // every second;
     }
 }
 
-let Timers = [new timer(30, 'Timer'), new timer(60, 'Timer2')];
+function myFunction(mins = 60, timerID = "timer", id) {
+    minutes[id] = mins;
+    let formattedMins = Math.floor(minutes[id]/60);
+    let formattedSecs = Math.floor(minutes[id]%60);
+    document.getElementById(timerID).innerHTML = formattedMins + " " + formattedSecs;
+    if (minutes[id] > 0 ) { // so it doesn't go to -1
+        minutes[id]--;
+    }
+    else if (minutes[id] === 0) {
+        minutes[id] = mins;
+    }
+}
+
+let resetTimer = (timerID = "timer", id) => {
+    isOn[id]=false;
+    firstClick[id]=true;
+    minutes[id]=null;
+    clearInterval(timer[id]);
+    document.getElementById(timerID).innerHTML = null;
+}
