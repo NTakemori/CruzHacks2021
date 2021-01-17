4eca008c3c9104295f66b4a4e2f080089b76adb6
Winson Chen <winsonchen108@gmail.com>
2021-01-17 12:07:30 +0800

    creating a new video page to play video

--- main.js
+++ main.js
@@ -4,6 +4,36 @@ const { app, BrowserWindow, Tray, ipcMain } = require('electron');
 let mainWindow = null;
 let tray = null;
 
+async function getAPI(){
+  const response = await fetch('http://127.0.0.1:5000/api/workout');
+  var data = await response.json();
+
+  console.log(data["videoID"].toString())
+  var link = "https://www.youtube.com/embed/" + data['videoID'].toString() + "?autoplay=1"; 
+  //console.log(link);
+  
+
+  let myIframe = document.getElementById("myIframe");
+  console.log(link);
+  myIframe.src = link;
+
+
+}
+
+// function getAPI(){
+//   var ID;
+//   fetch('http://127.0.0.1:5000/api/med') 
+//   .then(res => res.json()) 
+//   .then(data => ID = data)
+
+//   console.log(ID);
+ 
+//   let link = "https://www.youtube.com/embed/" + ID['videoID'] + "?autoplay=1"; 
+//   console.log(link);
+//   return link;
+  
+// }
+
 function createWindow () {
   mainWindow = new BrowserWindow({
     width: 800,
